30ee664488958de7734ca02b543c3d50881b70bc
Pham Hoai <hoai04042000@gmail.com>
2021-08-23 22:52:53 +0700

    Add Basic dialog

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,82 @@
 <template>
-    <!-- 
+    <div v-if="isShow">
+        <!-- 
         - Đây sử dụng ref này
         - Sử dụng props: truyền dữ liệu từ component cha sang componet con nhá !
 
      -->
-    <HelloWorld ref="hello" :titeNameNhe=namesss @DoNhaEmit="emitNha()" />
-    <!--nếu mà viết như trên là kiểu v-bind khi mà namesss thay đổi thì titeNameNhe cũng thay đổi theo còn viết theo kiểu dưới thì là cố định là " Xinh gái quá đê"
+        <HelloWorld ref="hello" :titeNameNhe=namesss @DoNhaEmit="emitNha()" />
+        <!--nếu mà viết như trên là kiểu v-bind khi mà namesss thay đổi thì titeNameNhe cũng thay đổi theo còn viết theo kiểu dưới thì là cố định là " Xinh gái quá đê"
                 titeNameNhe= "Xinh gái quá đê " 
         
     -->
 
-
-
-    <!-- Ví dụ về v-bind  -->
-    <div :class=" {colorToggle: number  === 70 } "> Số liệu: {{number }}</div>
-    <!-- Hoặc có kiểu viết kiểu khác là 
+        <!-- Ví dụ về v-bind  -->
+        <div :class=" {colorToggle: number  === 70 } "> Số liệu: {{number }}</div>
+        <!-- Hoặc có kiểu viết kiểu khác là 
         - có thể thuộc kiểu true false bình thường như :class = "{ colorToggle: true}"
         - có thể thuộc kiểu :class="{ active: isActive, 'text-danger': hasError }" 1 nhóm các class chứ không phải chỉ chứa 1
     -->
-    <button @click="changeNumber($event)"> ChangeNumber </button>
+        <button @click="changeNumber($event)"> ChangeNumber </button>
 
-    <!-- Nếu mà ta không gì ở đây mặc định nó default -->
-    <button @click="testRef($event)"> TestRefComponet</button>
+        <!-- Nếu mà ta không gì ở đây mặc định nó default -->
+        <button @click="testRef($event)"> TestRefComponet</button>
 
 
 
-    <!-- Cần phải phân biệt giữa v-if và v-show
+        <!-- Cần phải phân biệt giữa v-if và v-show
         - v-if: thì là nó đúng nó sẽ hiển thị
         - v-show: nó render( hiển thị) ra rồi nhưng mà do thuộc tính style nó ẩn đi thôi chứ thực chất nó vẫn ở đấy 
 
         Đã lấy ví dụ để xem ở bên dưới là xem ở Elements của trình duyệt để thấy được sự khác biệt này
     -->
-    <div v-if="isClass" class="card" @mousemove="changeXY">
-        X tọa độ là: {{X}} <br>
-        Y tọa độ là: {{Y}}
+        <div v-if="isClass" class="card" @mousemove="changeXY">
+            X tọa độ là: {{X}} <br>
+            Y tọa độ là: {{Y}}
 
-    </div>
-    <div v-show="isClass" class="card"> </div>
-    
-    <!-- Cái này dùng để test xem nhúng được bootstrap chưa thôi -->
-    <!-- Nhưng nó chưa hỗ trợ hết cho vue 3 hay sao ấy (stackoverflow) -->
-    <div class="col-md-12">
-        <div class="row">
-            <div class="col-md-4">xinh gái</div>
-            <div class="col-md-4">xinh gái 2</div>
-            <div class="col-md-4">xinh gái 3</div>
+        </div>
+        <div v-show="isClass" class="card"> </div>
+
+        <!-- Cái này dùng để test xem nhúng được bootstrap chưa thôi -->
+        <!-- Nhưng nó chưa hỗ trợ hết cho vue 3 hay sao ấy (stackoverflow) -->
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4">xinh gái</div>
+                <div class="col-md-4">xinh gái 2</div>
+                <div class="col-md-4">xinh gái 3</div>
+            </div>
         </div>
     </div>
+
+    <BaseDialog v-if="isShowBaseDialog" @cancelssss ="changBasicDialog()">
+        <!-- Có thể đặt thứ tự linh tinh cũng được -->
+        <template v-slot:footer >
+            <h1>Dữ liệu từ Appp nha footer</h1>
+        </template>
+        <template v-slot:headerrrr>
+            <h1>Dữ liệu từ App nha Header</h1>
+        </template>
+
+        <!-- còn nếu mặc định nó không trong template nào thì nó ở trong cái BasicDialog có mỗi chữ <slot/> -->
+        <h1>Thế nhé slot không ở trong các template nào</h1>
+        <button @click= " changBasicDialog()">XinhgaiApp</button>
+        <br>
+        <br>
+        
+    </BaseDialog>
+    <button @click= " changBasicDialog()">Toggle</button>
 
 </template>
 
 <script>
     import HelloWorld from './components/HelloWorld.vue'
+    import BaseDialog from './components/BaseDialog.vue'
 
     export default {
         name: 'App',
         components: {
-            HelloWorld
+            HelloWorld,
+            BaseDialog
         },
         data() {
             return {
@@ -65,6 +85,8 @@
                 X: 0,
                 Y: 0,
                 namesss: "hoaoooooooooo",
+                isShow: false,
+                isShowBaseDialog: false,
             }
         },
         methods: {
@@ -94,6 +116,13 @@
             },
             emitNha() {
                 alert("truyền emit từ con sang cha");
+            },
+            changBasicDialog(){
+                this.isShowBaseDialog = !this.isShowBaseDialog;
+                console.log( this.isShowBaseDialog);
+            },
+            changeToggle(){
+
             }
         },
     }
---
+++ src/components/BaseDialog.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="dialog-backdrop" @click.self="changeActive()">
+        <div class="dialog-container">
+            <div class="header">
+                header
+                <slot  name="headerrrr" /> 
+            </div>
+            <div class="content">
+                <div>Cuộc đời tôi</div>
+                <slot />
+                <button @click="changeActive()">Xiinh gái BasicDialog</button>
+            </div>
+            <div class="footer">
+                footer
+                <slot  name="footer" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BaseDialog",
+        props: {
+            // active: { type: Boolean, default: true }
+        },
+        methods: {
+            changeActive() {
+                this.$emit("cancelssss");
+            }
+        }
+    };
+</script>
+
+<style>
+    .dialog-backdrop {
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .dialog-container {
+        max-width: 768px;
+        box-shadow: 0 11px 15px -7px rgba(0, 0, 0, 0.2), 0 24px 38px 3px rgba(0, 0, 0, 0.14), 0 9px 46px 8px rgba(0, 0, 0, 0.12) !important;
+        margin-top: 30px;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 15px;
+        background-color: #fff !important;
+    }
+
+    .dialog-enter-active,
+    .dialog-leave-active {
+        transition: opacity 0.2s;
+    }
+
+    .dialog-enter,
+    .dialog-leave-to {
+        opacity: 0 !important;
+    }
+
+    .dialog-enter-active .dialog-container,
+    .dialog-leave-active .dialog-container {
+        transition: transform 0.4s;
+    }
+
+    .dialog-enter .dialog-container,
+    .dialog-leave-to .dialog-container {
+        transform: scale(0.9);
+    }
+
+    .header {
+        background-color: yellowgreen !important;
+    }
+
+    .content {
+        background-color: aquamarine !important;
+    }
+
+    .footer {
+        background-color: blueviolet !important;
+    }
+</style>
